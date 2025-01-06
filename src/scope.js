7eb9d4484c559198ab0185cead0b062d9a5347c6
axelquartz <axelarep@gmail.com>
2025-01-06 12:48:07 -0600

    Lexical scope

--- src/scope.js
+++ src/scope.js
@@ -6,13 +6,42 @@
 // scope();
 // console.log(origin);
 
-let name = "Axel";
+// let name = "Axel";
 
-function greet() {
-  name = "Riki";
-  if (name === "Riki") {
-    console.log(`Hello ${name}`);
+// function greet() {
+//   name = "Riki";
+//   if (name === "Riki") {
+//     console.log(`Hello ${name}`);
+//   }
+// }
+
+// greet();
+
+// "use strict";
+
+// axel = "Axel";
+// console.log(axel);
+
+const myGlobal = 0;
+
+function myFunction() {
+  const myNumber = 1;
+  console.log(myGlobal);
+
+  function parent() {
+    const inner = 2;
+    console.log(myNumber);
+    console.log(myGlobal);
+
+    function child() {
+      console.log(inner);
+      console.log(myNumber);
+      console.log(myGlobal);
+    }
+    return child();
   }
+  return parent();
 }
+// Inner function
 
-greet();
+myFunction();
