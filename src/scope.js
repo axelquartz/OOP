a1b815cb75f62464916df2277c0dc82f14e3466c
axelquartz <axelarep@gmail.com>
2025-01-07 11:45:24 -0600

    Closure with Money Box exercise

--- src/scope.js
+++ src/scope.js
@@ -22,26 +22,67 @@
 // axel = "Axel";
 // console.log(axel);
 
-const myGlobal = 0;
+// const myGlobal = 0;
 
-function myFunction() {
-  const myNumber = 1;
-  console.log(myGlobal);
+// function myFunction() {
+//   const myNumber = 1;
+//   console.log(myGlobal);
 
-  function parent() {
-    const inner = 2;
-    console.log(myNumber);
-    console.log(myGlobal);
+//   function parent() {
+//     const inner = 2;
+//     console.log(myNumber);
+//     console.log(myGlobal);
 
-    function child() {
-      console.log(inner);
-      console.log(myNumber);
-      console.log(myGlobal);
-    }
-    return child();
+//     function child() {
+//       console.log(inner);
+//       console.log(myNumber);
+//       console.log(myGlobal);
+//     }
+//     return child();
+//   }
+//   return parent();
+// }
+// // Inner function
+
+// myFunction();
+
+// function sayHello() {
+//   const name = "Axel";
+//   function sayName() {
+//     console.log(name);
+//   }
+//   // function sayAge() {
+//   //   console.log(30);
+//   // }
+//   // return sayAge;
+//   return sayName;
+// }
+
+// const hello = sayHello();
+// hello();
+
+// function moneyBox(coins) {
+//   let saveCoins = 0;
+//   saveCoins += coins;
+//   console.log(`MoneyBox: $${saveCoins}`);
+//   return saveCoins;
+// }
+
+// moneyBox(10);
+// moneyBox(20);
+// moneyBox(30);
+
+function moneyBox() {
+  let saveCoins = 0;
+  function countCoins(coins) {
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+    return saveCoins;
   }
-  return parent();
+  return countCoins;
 }
-// Inner function
 
-myFunction();
+const myMoneyBox = moneyBox();
+myMoneyBox(10);
+myMoneyBox(20);
+myMoneyBox(30);
