41fb9f5bf433aa3ab0090bdad88c648a3ae7acbb
axelquartz <axelarep@gmail.com>
2025-01-08 23:28:55 -0600

    Object seal and freeze

--- src/OOPMid.js
+++ src/OOPMid.js
@@ -27,14 +27,45 @@ const axel = {
   },
 };
 
-// console.log(Object.keys(axel));
-// console.log(Object.getOwnPropertyNames(axel));
-// console.log(Object.entries(axel));
-console.log(Object.getOwnPropertyDescriptors(axel));
-
-Object.defineProperty(axel, "NASAtest", {
-  value: "Aliens",
+Object.defineProperty(axel, "country", {
+  value: "Mexico",
   enumerable: true,
   configurable: true,
   writable: true,
 });
+
+Object.defineProperty(axel, "OS", {
+  value: "Ubuntu",
+  enumerable: false,
+  configurable: true,
+  writable: true,
+});
+
+Object.defineProperty(axel, "navigator", {
+  value: "Chrome",
+  enumerable: true,
+  configurable: false,
+  writable: true,
+});
+
+Object.defineProperty(axel, "editor", {
+  value: "VSCode",
+  enumerable: true,
+  configurable: true,
+  writable: false,
+});
+
+Object.defineProperty(axel, "language", {
+  value: "English",
+  enumerable: false,
+  configurable: false,
+  writable: false,
+});
+
+Object.seal(axel);
+
+// console.log(Object.keys(axel));
+// console.log(Object.getOwnPropertyNames(axel));
+// console.log(Object.entries(axel));
+console.log(Object.getOwnPropertyDescriptors(axel));
+console.log(Object.keys(axel));
