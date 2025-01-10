c3b5cceed89784a013db3ae8cd15d78efc71fccd
axelquartz <axelarep@gmail.com>
2025-01-10 12:35:54 -0600

    shallow copy in objects

--- src/OOPMid.js
+++ src/OOPMid.js
@@ -15,57 +15,86 @@
 // Person.sayName();
 // axel.sayName();
 
+// const axel = {
+//   name: "Axel",
+//   age: 30,
+//   approvedCourses: ["JavaScript", "HTML", "CSS"],
+//   addCourse(newCourse) {
+//     console.log(this);
+//     console.log(this.approvedCourses);
+
+//     this.approvedCourses.push(newCourse);
+//   },
+// };
+
+// Object.defineProperty(axel, "country", {
+//   value: "Mexico",
+//   enumerable: true,
+//   configurable: true,
+//   writable: true,
+// });
+
+// Object.defineProperty(axel, "OS", {
+//   value: "Ubuntu",
+//   enumerable: false,
+//   configurable: true,
+//   writable: true,
+// });
+
+// Object.defineProperty(axel, "navigator", {
+//   value: "Chrome",
+//   enumerable: true,
+//   configurable: false,
+//   writable: true,
+// });
+
+// Object.defineProperty(axel, "editor", {
+//   value: "VSCode",
+//   enumerable: true,
+//   configurable: true,
+//   writable: false,
+// });
+
+// Object.defineProperty(axel, "language", {
+//   value: "English",
+//   enumerable: false,
+//   configurable: false,
+//   writable: false,
+// });
+
+// Object.seal(axel);
+
+// // console.log(Object.keys(axel));
+// // console.log(Object.getOwnPropertyNames(axel));
+// // console.log(Object.entries(axel));
+// console.log(Object.getOwnPropertyDescriptors(axel));
+// console.log(Object.keys(axel));
+
 const axel = {
   name: "Axel",
   age: 30,
-  approvedCourses: ["JavaScript", "HTML", "CSS"],
-  addCourse(newCourse) {
-    console.log(this);
-    console.log(this.approvedCourses);
-
-    this.approvedCourses.push(newCourse);
+  height: 1.8,
+  weight: 92,
+  interests: {
+    sport: "Basketball",
+    music: "Rock",
   },
 };
 
-Object.defineProperty(axel, "country", {
-  value: "Mexico",
-  enumerable: true,
-  configurable: true,
-  writable: true,
-});
+const riki = {};
+for (prop in axel) {
+  riki[prop] = axel[prop];
+}
 
-Object.defineProperty(axel, "OS", {
-  value: "Ubuntu",
-  enumerable: false,
-  configurable: true,
-  writable: true,
-});
+axel.skinColor = "Brown";
+riki.interests.music = "Pop";
 
-Object.defineProperty(axel, "navigator", {
-  value: "Chrome",
-  enumerable: true,
-  configurable: false,
-  writable: true,
-});
+const laura = Object.create(axel);
 
-Object.defineProperty(axel, "editor", {
-  value: "VSCode",
-  enumerable: true,
-  configurable: true,
-  writable: false,
-});
+laura.__proto__.interests.music = "Jazz";
 
-Object.defineProperty(axel, "language", {
-  value: "English",
-  enumerable: false,
-  configurable: false,
-  writable: false,
-});
+console.log(laura);
 
-Object.seal(axel);
-
-// console.log(Object.keys(axel));
-// console.log(Object.getOwnPropertyNames(axel));
-// console.log(Object.entries(axel));
-console.log(Object.getOwnPropertyDescriptors(axel));
-console.log(Object.keys(axel));
+console.log(laura.interests.music);
+console.log(axel.interests.music);
+console.log(riki.interests.music);
